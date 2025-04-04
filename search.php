a4a612772473cc75ea979699b4f60bfd34a98762
sobujmiah01 <fsobujmiah@gmail.com>
2025-04-04 08:41:59 +0600

    release modern design

--- search.php
+++ search.php
@@ -14,8 +14,22 @@
                 <?php custom_pagination(); ?>
             </div>
         <?php else : ?>
-            <div class="no-results">
-                <p><?php esc_html_e('Sorry, no results found.', 'webdescode'); ?></p>
+            <div class="no-results-wrapper">
+                <div class="no-results-content">
+                    <svg class="no-results-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        <line x1="11" y1="8" x2="11" y2="14"></line>
+                        <line x1="8" y1="11" x2="14" y2="11"></line>
+                    </svg>
+                    <h3 class="no-results-title"><?php esc_html_e('Oops! No Results Found', 'webdescode'); ?></h3>
+                    <p class="no-results-message"><?php esc_html_e("We couldn't find what you're looking for. Try different keywords or browse our categories.", 'webdescode'); ?></p>
+                    <div class="no-results-action">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="no-results-button">
+                            <?php esc_html_e('Return Home', 'webdescode'); ?>
+                        </a>
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
     </article>
